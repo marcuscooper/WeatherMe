052610bcc869711257862bf0ecf0b05c3871fd67
Marcus Cooper <marcuscooper@Marcuss-MacBook-Pro.local>
2016-06-12 00:45:49 -0400

    Initial Storyboard Layout

--- WeatherMe/ViewController.swift
+++ WeatherMe/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let degree = "\u{00B0}" // degree symbol
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
