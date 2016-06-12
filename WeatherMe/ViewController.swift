ca7d3c8550168d87d98941208fbc1841268119db
Marcus Cooper <marcuscooper@Marcuss-MacBook-Pro.local>
2016-06-12 14:00:07 -0400

    Wired up VC to Storyboard

--- WeatherMe/ViewController.swift
+++ WeatherMe/ViewController.swift
@@ -10,18 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityNameLbl: UILabel!
+    @IBOutlet weak var currentWeatherImg: UIImageView!
+    @IBOutlet weak var currentTempLbl: UILabel!
+    @IBOutlet weak var currentTimeLbl: UILabel!
+    @IBOutlet weak var currentDayLbl: UILabel!
+    @IBOutlet weak var thermometerImg: UIImageView!
+    @IBOutlet weak var todayHighTempLbl: UILabel!
+    @IBOutlet weak var todayLowTempLbl: UILabel!
+    @IBOutlet weak var percipitationChanceLbl: UILabel!
+    
+    @IBOutlet weak var dayOneDayLbl: UILabel!
+    @IBOutlet weak var dayOneWeatherImg: UIImageView!
+    @IBOutlet weak var dayOneHighTempLbl: UILabel!
+    @IBOutlet weak var dayOneLowTempLbl: UILabel!
+    
+    @IBOutlet weak var dayTwoDayLbl: UILabel!
+    @IBOutlet weak var dayTwoWeatherImg: UIImageView!
+    @IBOutlet weak var dayTwoHighTempLbl: UILabel!
+    @IBOutlet weak var dayTwoLowTempLbl: UILabel!
+    
+    @IBOutlet weak var dayThreeDayLbl: UILabel!
+    @IBOutlet weak var dayThreeWeatherImg: UIImageView!
+    @IBOutlet weak var dayThreeHighTempLbl: UILabel!
+    @IBOutlet weak var dayThreeLowTempLbl: UILabel!
+    
+    @IBOutlet weak var dayFourDayLbl: UILabel!
+    @IBOutlet weak var dayFourWeatherImg: UIImageView!
+    @IBOutlet weak var dayFourHighTempLbl: UILabel!
+    @IBOutlet weak var dayFourLowTempLbl: UILabel!
+    
+    @IBOutlet weak var dayFiveDayLbl: UILabel!
+    @IBOutlet weak var dayFiveWeatherImg: UIImageView!
+    @IBOutlet weak var dayFiveHighTempImg: UILabel!
+    @IBOutlet weak var dayFiveLowTempImg: UILabel!
+    
     let degree = "\u{00B0}" // degree symbol
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func didPressSunRiseSetBtn(sender: AnyObject) {
     }
-
-
 }
 
