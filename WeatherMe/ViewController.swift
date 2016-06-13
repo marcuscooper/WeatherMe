062ad69e38547d874adb2b19e17db1789cf885b1
Marcus Cooper <marcuscooper@Marcuss-MacBook-Pro.local>
2016-06-13 00:17:54 -0400

    Wired up forecast data

--- WeatherMe/ViewController.swift
+++ WeatherMe/ViewController.swift
@@ -43,45 +43,83 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var dayFiveDayLbl: UILabel!
     @IBOutlet weak var dayFiveWeatherImg: UIImageView!
-    @IBOutlet weak var dayFiveHighTempImg: UILabel!
-    @IBOutlet weak var dayFiveLowTempImg: UILabel!
+    @IBOutlet weak var dayFiveHighTempLbl: UILabel!
+    @IBOutlet weak var dayFiveLowTempLbl: UILabel!
     
     var cityName: String!
     var weatherInstance: WeatherInstance!
+    var currentDate: String!
     var currentTime: String!
+    var dayPlusOne: String!
+    var dayPlusTwo: String!
+    var dayPlusThree: String!
+    var dayPlusFour: String!
+    var dayPlusFive: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         cityName = "28173"
         weatherInstance = WeatherInstance.init(cityName: cityName)
         
-        let styler = NSDateFormatter()
-        styler.timeStyle = NSDateFormatterStyle.ShortStyle
-        currentTime = styler.stringFromDate(NSDate())
+        let timeStyler = NSDateFormatter()
+        timeStyler.timeStyle = NSDateFormatterStyle.ShortStyle
+        currentTime = timeStyler.stringFromDate(NSDate())
+        
+        let dateStyler = NSDateFormatter()
+        dateStyler.dateStyle = NSDateFormatterStyle.ShortStyle
+        currentDate = dateStyler.stringFromDate(NSDate())
+        
+        getNextFiveWeekdays()
+        dayOneDayLbl.text = dayPlusOne
+        dayTwoDayLbl.text = dayPlusTwo
+        dayThreeDayLbl.text = dayPlusThree
+        dayFourDayLbl.text = dayPlusFour
+        dayFiveDayLbl.text = dayPlusFive
         
         weatherInstance.downloadCurentWeatherDetails { () -> () in
             //this will be called after download is done
-            self.uodateUI()
+            self.updateUI()
         }
         
         weatherInstance.downloadFutureWeatherDetails { () -> () in
             //this will be called after download is done
-            self.uodateUI()
+            self.updateBottomUI()
         }
     }
     
-    func uodateUI() {
+    func updateUI() {
         cityNameLbl.text = weatherInstance.cityName
         currentWeatherImg.image = UIImage(named: weatherInstance.weatherType.rawValue)
         currentTempLbl.text = weatherInstance.temp
-        currentDayLbl.text = getDayOfWeekString("2016-06-12")
+        currentDayLbl.text = getDayOfWeekString(currentDate)
         currentTimeLbl.text = currentTime
         todayHighTempLbl.text = weatherInstance.highTemp
         todayLowTempLbl.text = weatherInstance.lowTemp
         windSpeedLbl.text = weatherInstance.windSpeed
         humidityLbl.text = weatherInstance.humidity
-        
     }
+    
+    func updateBottomUI() {
+        dayOneWeatherImg.image = UIImage(named: weatherInstance.firstWeatherType.rawValue)
+        dayOneLowTempLbl.text = weatherInstance.firstLowTemp
+        dayOneHighTempLbl.text = weatherInstance.firstHighTemp
+
+        dayTwoWeatherImg.image = UIImage(named: weatherInstance.secondWeatherType.rawValue)
+        dayTwoLowTempLbl.text = weatherInstance.secondLowTemp
+        dayTwoHighTempLbl.text = weatherInstance.secondHighTemp
+
+        dayThreeWeatherImg.image = UIImage(named: weatherInstance.thirdWeatherType.rawValue)
+        dayThreeLowTempLbl.text = weatherInstance.thirdLowTemp
+        dayThreeHighTempLbl.text = weatherInstance.thirdHighTemp
+
+        dayFourWeatherImg.image = UIImage(named: weatherInstance.fourthWeatherType.rawValue)
+        dayFourLowTempLbl.text = weatherInstance.fourthLowTemp
+        dayFourHighTempLbl.text = weatherInstance.fourthHighTemp
+
+        dayFiveWeatherImg.image = UIImage(named: weatherInstance.fifthWeatherType.rawValue)
+        dayFiveLowTempLbl.text = weatherInstance.fifthLowTemp
+        dayFiveHighTempLbl.text = weatherInstance.fifthHighTemp
+}
     
     @IBAction func didPressSunRiseSetBtn(sender: AnyObject) {
         
@@ -89,7 +127,7 @@ class ViewController: UIViewController {
     
     func getDayOfWeekString(today:String)->String? {
         let formatter  = NSDateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
+        formatter.dateFormat = "M/dd/YY"
         if let todayDate = formatter.dateFromString(today) {
             let myCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
             let myComponents = myCalendar.components(.Weekday, fromDate: todayDate)
@@ -115,6 +153,62 @@ class ViewController: UIViewController {
             }
         } else {
             return nil
+        }
+    }
+    
+    func getNextFiveWeekdays() {
+        print(currentDate)
+        print(getDayOfWeekString(currentDate))
+        
+        switch getDayOfWeekString(currentDate)! {
+            case "Sunday":
+                dayPlusOne = "MON"
+                dayPlusTwo = "TUE"
+                dayPlusThree = "WED"
+                dayPlusFour = "THU"
+                dayPlusFive = "FRI"
+            case "Monday":
+                dayPlusOne = "TUE"
+                dayPlusTwo = "WED"
+                dayPlusThree = "ThU"
+                dayPlusFour = "FRI"
+                dayPlusFive = "SAT"
+            case "Tuesday":
+                dayPlusOne = "WED"
+                dayPlusTwo = "THU"
+                dayPlusThree = "FRI"
+                dayPlusFour = "SAT"
+                dayPlusFive = "SUN"
+            case "Wednesday":
+                dayPlusOne = "THU"
+                dayPlusTwo = "FRI"
+                dayPlusThree = "SAT"
+                dayPlusFour = "SUN"
+                dayPlusFive = "MON"
+            case "Thursday":
+                dayPlusOne = "FRI"
+                dayPlusTwo = "SAT"
+                dayPlusThree = "SUN"
+                dayPlusFour = "MON"
+                dayPlusFive = "TUE"
+            case "Friday":
+                dayPlusOne = "SAT"
+                dayPlusTwo = "SUN"
+                dayPlusThree = "MON"
+                dayPlusFour = "TUE"
+                dayPlusFive = "WED"
+            case "Saturday":
+                dayPlusOne = "SUN"
+                dayPlusTwo = "MON"
+                dayPlusThree = "TUE"
+                dayPlusFour = "WED"
+                dayPlusFive = "THU"
+            default:
+                dayPlusOne = ""
+                dayPlusTwo = ""
+                dayPlusThree = ""
+                dayPlusFour = ""
+                dayPlusFive = ""
         }
     }
 }
--- WeatherMe/WeatherInstance.swift
+++ WeatherMe/WeatherInstance.swift
@@ -34,6 +34,26 @@ class WeatherInstance {
     private var _currentWeatherUrl: String!
     private var _forecastWeatherUrl: String!
     
+    private var _firstWeatherType: weatherTypeEnum!
+    private var _firstHighTemp: String!
+    private var _firstLowTemp: String!
+    
+    private var _secondWeatherType: weatherTypeEnum!
+    private var _secondHighTemp: String!
+    private var _secondLowTemp: String!
+    
+    private var _thirdWeatherType: weatherTypeEnum!
+    private var _thirdHighTemp: String!
+    private var _thirdLowTemp: String!
+    
+    private var _fourthWeatherType: weatherTypeEnum!
+    private var _fourthHighTemp: String!
+    private var _fourthLowTemp: String!
+    
+    private var _fifthWeatherType: weatherTypeEnum!
+    private var _fifthHighTemp: String!
+    private var _fifthLowTemp: String!
+    
     var cityName: String {
         return _cityName
     }
@@ -60,6 +80,66 @@ class WeatherInstance {
     
     var humidity: String {
         return _humidity
+    }
+    
+    var firstWeatherType: weatherTypeEnum {
+        return _firstWeatherType
+    }
+    
+    var firstHighTemp: String {
+        return _firstHighTemp
+    }
+    
+    var firstLowTemp: String {
+        return _firstLowTemp
+    }
+    
+    var secondWeatherType: weatherTypeEnum {
+        return _secondWeatherType
+    }
+    
+    var secondHighTemp: String {
+        return _secondHighTemp
+    }
+    
+    var secondLowTemp: String {
+        return _secondLowTemp
+    }
+    
+    var thirdWeatherType: weatherTypeEnum {
+        return _thirdWeatherType
+    }
+    
+    var thirdHighTemp: String {
+        return _thirdHighTemp
+    }
+    
+    var thirdLowTemp: String {
+        return _thirdLowTemp
+    }
+    
+    var fourthWeatherType: weatherTypeEnum {
+        return _fourthWeatherType
+    }
+    
+    var fourthHighTemp: String {
+        return _fourthHighTemp
+    }
+    
+    var fourthLowTemp: String {
+        return _fourthLowTemp
+    }
+    
+    var fifthWeatherType: weatherTypeEnum {
+        return _fifthWeatherType
+    }
+    
+    var fifthHighTemp: String {
+        return _fifthHighTemp
+    }
+    
+    var fifthLowTemp: String {
+        return _fifthLowTemp
     }
     
     let degree = "\u{00B0}" // degree symbol
@@ -89,14 +169,25 @@ class WeatherInstance {
                 
                 if let weatherDict = dict["weather"] as? [Dictionary<String, AnyObject>] where weatherDict.count > 0 {
                     if let main = weatherDict[0]["main"] as? String {
-                        if main == "Clear" {
-                            if self.isDay() {
+                        switch main {
+                            case "Clear":
+                                if self.isDay() {
+                                    self._weatherType = weatherTypeEnum.ClearDay
+                                } else {
+                                    self._weatherType = weatherTypeEnum.ClearNight
+                                }
+                            case "Rain":
+                                self._weatherType = weatherTypeEnum.Rain
+                            case "Clouds":
+                                self._weatherType = weatherTypeEnum.Cloudy
+                            case "Thunderstorm":
+                                self._weatherType = weatherTypeEnum.ThunderShowers
+                            case "Snow":
+                                self._weatherType = weatherTypeEnum.Snow
+                            case "Hail":
+                                self._weatherType = weatherTypeEnum.Hail
+                            default:
                                 self._weatherType = weatherTypeEnum.ClearDay
-                            } else {
-                                self._weatherType = weatherTypeEnum.ClearNight
-                            }
-                        } else {
-                            self._weatherType = weatherTypeEnum.Hail
                         }
                         
                         print("Weather Type: \(self._weatherType.rawValue)")
@@ -152,10 +243,205 @@ class WeatherInstance {
             if let dict = result.value as? Dictionary<String, AnyObject> {
                 if let listDict = dict["list"] as? [Dictionary<String, AnyObject>] where listDict.count > 0 {
                     let day1Dict = listDict[0]
+                    if let weatherDict = day1Dict["weather"] as? [Dictionary<String, AnyObject>] where weatherDict.count > 0 {
+                        if let main = weatherDict[0]["main"] as? String {
+                            switch main {
+                            case "Clear":
+                                if self.isDay() {
+                                    self._firstWeatherType = weatherTypeEnum.ClearDay
+                                } else {
+                                    self._firstWeatherType = weatherTypeEnum.ClearNight
+                                }
+                            case "Rain":
+                                self._firstWeatherType = weatherTypeEnum.Rain
+                            case "Clouds":
+                                self._firstWeatherType = weatherTypeEnum.Cloudy
+                            case "Thunderstorm":
+                                self._firstWeatherType = weatherTypeEnum.ThunderShowers
+                            case "Snow":
+                                self._firstWeatherType = weatherTypeEnum.Snow
+                            case "Hail":
+                                self._firstWeatherType = weatherTypeEnum.Hail
+                            default:
+                                self._firstWeatherType = weatherTypeEnum.ClearDay
+                            }
+                            print("Weather Type: \(self._firstWeatherType.rawValue)")
+                        }
+                    }
+                    
+                    if let firstMainDict = day1Dict["main"] as? Dictionary<String, AnyObject>
+                    {
+                        if let firstLowTemp = firstMainDict["temp_min"] as? Double {
+                            self._firstLowTemp = ("\(Int(round(firstLowTemp)))\(self.degree)")
+                            print("Low Temp(1): \(self._firstLowTemp)")
+                        }
+                        
+                        if let firstHighTemp = firstMainDict["temp_max"] as? Double  {
+                            self._firstHighTemp = ("\(Int(round(firstHighTemp)))\(self.degree)")
+                            print("High Temp(1): \(self._firstHighTemp)")
+                        }
+                    }
+                    
                     let day2Dict = listDict[8]
+                    if let weatherDict = day2Dict["weather"] as? [Dictionary<String, AnyObject>] where weatherDict.count > 0 {
+                        if let main = weatherDict[0]["main"] as? String {
+                            switch main {
+                            case "Clear":
+                                if self.isDay() {
+                                    self._secondWeatherType = weatherTypeEnum.ClearDay
+                                } else {
+                                    self._secondWeatherType = weatherTypeEnum.ClearNight
+                                }
+                            case "Rain":
+                                self._secondWeatherType = weatherTypeEnum.Rain
+                            case "Clouds":
+                                self._secondWeatherType = weatherTypeEnum.Cloudy
+                            case "Thunderstorm":
+                                self._secondWeatherType = weatherTypeEnum.ThunderShowers
+                            case "Snow":
+                                self._secondWeatherType = weatherTypeEnum.Snow
+                            case "Hail":
+                                self._secondWeatherType = weatherTypeEnum.Hail
+                            default:
+                                self._secondWeatherType = weatherTypeEnum.ClearDay
+                            }
+                            print("Weather Type: \(self._secondWeatherType.rawValue)")
+                        }
+                    }
+                    
+                    if let secondMainDict = day2Dict["main"] as? Dictionary<String, AnyObject>
+                    {
+                        if let secondLowTemp = secondMainDict["temp_min"] as? Double {
+                            self._secondLowTemp = ("\(Int(round(secondLowTemp)))\(self.degree)")
+                            print("Low Temp(2): \(self._secondLowTemp)")
+                        }
+                        
+                        if let secondHighTemp = secondMainDict["temp_max"] as? Double  {
+                            self._secondHighTemp = ("\(Int(round(secondHighTemp)))\(self.degree)")
+                            print("High Temp(2): \(self._secondHighTemp)")
+                        }
+                    }
+                    
                     let day3Dict = listDict[16]
+                    if let weatherDict = day3Dict["weather"] as? [Dictionary<String, AnyObject>] where weatherDict.count > 0 {
+                        if let main = weatherDict[0]["main"] as? String {
+                            switch main {
+                            case "Clear":
+                                if self.isDay() {
+                                    self._thirdWeatherType = weatherTypeEnum.ClearDay
+                                } else {
+                                    self._thirdWeatherType = weatherTypeEnum.ClearNight
+                                }
+                            case "Rain":
+                                self._thirdWeatherType = weatherTypeEnum.Rain
+                            case "Clouds":
+                                self._thirdWeatherType = weatherTypeEnum.Cloudy
+                            case "Thunderstorm":
+                                self._thirdWeatherType = weatherTypeEnum.ThunderShowers
+                            case "Snow":
+                                self._thirdWeatherType = weatherTypeEnum.Snow
+                            case "Hail":
+                                self._thirdWeatherType = weatherTypeEnum.Hail
+                            default:
+                                self._thirdWeatherType = weatherTypeEnum.ClearDay
+                            }
+                            print("Weather Type: \(self._thirdWeatherType.rawValue)")
+                        }
+                    }
+                    
+                    if let thirdMainDict = day3Dict["main"] as? Dictionary<String, AnyObject>
+                    {
+                        if let thirdLowTemp = thirdMainDict["temp_min"] as? Double {
+                            self._thirdLowTemp = ("\(Int(round(thirdLowTemp)))\(self.degree)")
+                            print("Low Temp(3): \(self._thirdLowTemp)")
+                        }
+                        
+                        if let thirdHighTemp = thirdMainDict["temp_max"] as? Double  {
+                            self._thirdHighTemp = ("\(Int(round(thirdHighTemp)))\(self.degree)")
+                            print("High Temp(3): \(self._thirdHighTemp)")
+                        }
+                    }
+                    
                     let day4Dict = listDict[24]
+                    if let weatherDict = day4Dict["weather"] as? [Dictionary<String, AnyObject>] where weatherDict.count > 0 {
+                        if let main = weatherDict[0]["main"] as? String {
+                            switch main {
+                            case "Clear":
+                                if self.isDay() {
+                                    self._fourthWeatherType = weatherTypeEnum.ClearDay
+                                } else {
+                                    self._fourthWeatherType = weatherTypeEnum.ClearNight
+                                }
+                            case "Rain":
+                                self._fourthWeatherType = weatherTypeEnum.Rain
+                            case "Clouds":
+                                self._fourthWeatherType = weatherTypeEnum.Cloudy
+                            case "Thunderstorm":
+                                self._fourthWeatherType = weatherTypeEnum.ThunderShowers
+                            case "Snow":
+                                self._fourthWeatherType = weatherTypeEnum.Snow
+                            case "Hail":
+                                self._fourthWeatherType = weatherTypeEnum.Hail
+                            default:
+                                self._fourthWeatherType = weatherTypeEnum.ClearDay
+                            }
+                            print("Weather Type: \(self._fourthWeatherType.rawValue)")
+                        }
+                    }
+                    
+                    if let fourthMainDict = day4Dict["main"] as? Dictionary<String, AnyObject>
+                    {
+                        if let fourthLowTemp = fourthMainDict["temp_min"] as? Double {
+                            self._fourthLowTemp = ("\(Int(round(fourthLowTemp)))\(self.degree)")
+                            print("Low Temp(4): \(self._fourthLowTemp)")
+                        }
+                        
+                        if let fourthHighTemp = fourthMainDict["temp_max"] as? Double  {
+                            self._fourthHighTemp = ("\(Int(round(fourthHighTemp)))\(self.degree)")
+                            print("High Temp(4): \(self._fourthHighTemp)")
+                        }
+                    }
+                    
                     let day5Dict = listDict[32]
+                    if let weatherDict = day5Dict["weather"] as? [Dictionary<String, AnyObject>] where weatherDict.count > 0 {
+                        if let main = weatherDict[0]["main"] as? String {
+                            switch main {
+                            case "Clear":
+                                if self.isDay() {
+                                    self._fifthWeatherType = weatherTypeEnum.ClearDay
+                                } else {
+                                    self._fifthWeatherType = weatherTypeEnum.ClearNight
+                                }
+                            case "Rain":
+                                self._fifthWeatherType = weatherTypeEnum.Rain
+                            case "Clouds":
+                                self._fifthWeatherType = weatherTypeEnum.Cloudy
+                            case "Thunderstorm":
+                                self._fifthWeatherType = weatherTypeEnum.ThunderShowers
+                            case "Snow":
+                                self._fifthWeatherType = weatherTypeEnum.Snow
+                            case "Hail":
+                                self._fifthWeatherType = weatherTypeEnum.Hail
+                            default:
+                                self._fifthWeatherType = weatherTypeEnum.ClearDay
+                            }
+                            print("Weather Type: \(self._fifthWeatherType.rawValue)")
+                        }
+                    }
+                    
+                    if let fifthMainDict = day5Dict["main"] as? Dictionary<String, AnyObject>
+                    {
+                        if let fifthLowTemp = fifthMainDict["temp_min"] as? Double {
+                            self._fifthLowTemp = ("\(Int(round(fifthLowTemp)))\(self.degree)")
+                            print("Low Temp(5): \(self._fifthLowTemp)")
+                        }
+                        
+                        if let fifthHighTemp = fifthMainDict["temp_max"] as? Double  {
+                            self._fifthHighTemp = ("\(Int(round(fifthHighTemp)))\(self.degree)")
+                            print("High Temp(5): \(self._fifthHighTemp)")
+                        }
+                    }
+                    completed()
                 }
             }
         }
